38808fb17a602d941940cdb0a9cf3b5f028b182c
Zineb Hamman <zhh1001@alu.ubu.es>
2026-05-09 21:06:56 +0200

    Update cliente.sh

--- cliente.sh
+++ cliente.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo "Iniciando servidor Python..."
+
+python3 "ServidorCamara.py" &
+
+#esperar a que el servidor arranque
+sleep 2
+
+echo "Compilando cliente C++..."
+
+g++ cliente.cpp -o cliente
+
+echo "Ejecutando cliente..."
+
+./cliente
